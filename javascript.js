e860a03d0862dbecb249d01c92a7434ccfe8e881
Leonard Anton <124513400+LeonardAnton@users.noreply.github.com>
2025-07-09 21:41:33 +0300

    Add logic to play one round

--- javascript.js
+++ javascript.js
@@ -11,10 +11,45 @@ function getComputerChoice() {
         return "scissors";
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let choice = prompt("Rock, Paper or Scissors?");
 
     return choice.toLowerCase();
 }
 
 let humanScore = 0, computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice)
+        console.log("Draw!");
+
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors.")
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock.")
+        humanScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats Paper.")
+        humanScore++;
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock.")
+        computerScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper.")
+        computerScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors.")
+        computerScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
